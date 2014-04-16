dc7cb47ad80e6ad6e322be0da95be3ab552abf77
Andreas Haberberger <ahaberberger@haberberger.com>
2014-04-16 22:09:53 +0200

    Consolidated Storage

--- Services/RedisStorage.php
+++ Services/RedisStorage.php
@@ -8,13 +8,15 @@
 
 namespace Haberberger\Bundle\RedisStorageBundle\Services;
 use Haberberger\Bundle\RedisStorageBundle\Exceptions\RedisBundleException;
+use Haberberger\Bundle\URSBundle\Model\AbstractUrsModel;
 use Predis\Client;
+use Symfony\Component\DependencyInjection\ContainerAware;
 
 /**
  * Class RedisStorage
  * @package Haberberger\Bundle\RedisStorageBundle\Services
  */
-class RedisStorage 
+class RedisStorage
 {
     const PROTOCOL_REDIS = 'redis';
     const USER_DEFAULT = '';
@@ -22,6 +24,9 @@ class RedisStorage
     const HOST_DEFAULT = 'localhost';
     const PORT_DEFAULT = 6379;
     const DB_DEFAULT = 0;
+    const KEY_META_CLASSNAME = 'classname';
+    const KEY_META = 'meta';
+    const KEY_CONTENT = 'content';
 
     /** @var \Predis\Client  */
     protected $_redis;
@@ -72,6 +77,54 @@ class RedisStorage
             $values[] = $this->stringRead($key);
         }
         return $values;
+    }
+
+    public function getModel($id)
+    {
+        $key = sprintf('ATTRIBUTE_%s', $id);
+        $data = json_decode($this->stringRead($key), true);
+        $meta = $data[self::KEY_META];
+        $content = $data[self::KEY_CONTENT];
+        $classname = $meta[self::KEY_META_CLASSNAME];
+        $instance = $classname::fromArray($content);
+        return $instance;
+    }
+
+    /**
+     * @param AbstractUrsModel $model
+     * @return mixed
+     */
+    public function writeModel(AbstractUrsModel $model)
+    {
+        $key = sprintf('%s_%s', get_class($model), $model->getId());
+        $meta = [
+            self::KEY_META_CLASSNAME => get_class($model)
+        ];
+        $content = $model->toArray();
+        return $this->stringWrite(
+            $key,
+            json_encode([
+                self::KEY_META => $meta,
+                self::KEY_CONTENT => $content
+            ])
+        );
+    }
+
+    public function listModel($model)
+    {
+        //TODO Pattern quoten
+        $pattern = sprintf('%s_*', $model);
+        $entries = $this->stringGetByPattern($pattern);
+        $out = [];
+        foreach ($entries as $entry) {
+            $data = json_decode($entry, true);
+            $meta = $data[self::KEY_META];
+            $content = $data[self::KEY_CONTENT];
+            $classname = $meta[self::KEY_META_CLASSNAME];
+            $instance = $classname::fromArray($content);
+            $out[] = $instance;
+        }
+        return $out;
     }
 
     public static function parseRedisUrl($url)
